747f2f48efa6185658754f8cea9f153f9c21369a
oussTheCOder <122299585+oussTheCOder@users.noreply.github.com>
2024-06-14 23:52:40 +0100

    img issue build

--- public/assets/index.js
+++ public/assets/index.js
@@ -19,8 +19,6 @@ import disc02 from "./disc-02.svg";
 import chromecast from "./chrome-cast.svg";
 import sliders04 from "./sliders-04.svg";
 import loading from "./loading.png";
-import background from "./background.jpg";
-import curve from "./hero/curve.png";
 import robot from "./hero/robot.jpg";
 import heroBackground from "./hero/hero-background.jpg";
 import lines from "./pricing/lines.svg";
@@ -71,8 +69,7 @@ export {
   chromecast,
   sliders04,
   loading,
-  background,
-  curve,
+
   robot,
   heroBackground,
   lines,
