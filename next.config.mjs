024466bb1b1b89b7fc131e3695be4461e1dbd280
oussTheCOder <ouss.coder92@gmail.com>
2024-07-09 22:16:51 +0100

    try to fix redirect seo issue problem

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,26 @@ const nextConfig = {
     images: {
         domains: ['image.tmdb.org'],
     },
+    async redirects() {
+        return [
+          {
+            source: '/en/',
+            destination: '/en',
+            permanent: true,
+          },
+          {
+            source: '/de',
+            destination: '/',
+            permanent: true, // Use a 301 redirect for permanent redirection
+          },
+          {
+            source: '/de/',
+            destination: '/',
+            permanent: true, // Handle trailing slash as well
+          },
+        ];
+      },
+    trailingSlash: false,
 };
 
 export default nextConfig;
