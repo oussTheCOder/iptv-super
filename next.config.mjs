f0ab463f267f083c44b56dd84392ca1f6d0fe7e4
oussTheCOder <ouss.coder92@gmail.com>
2025-01-27 08:52:09 +0100

    translate update completed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,4 @@
-// /** @type {import('next').NextConfig} */
+
 // const nextConfig = {
 //     images: {
 //         domains: ['image.tmdb.org'],
@@ -26,20 +26,49 @@
 // };
 
 // export default nextConfig;
+// next.config.mjs
 
-module.exports = {
-  i18n: {
-    locales: ['de', 'en'],
-    defaultLocale: 'de',
-    localeDetection: false,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['image.tmdb.org'],
   },
-  async rewrites() {
+  async redirects() {
     return [
+      // Remove redirect for /en/ to /en
       {
-        source: '/:locale',
-        destination: '/:locale/home',
+        source: '/en/',
+        destination: '/en',
+        permanent: false,
+      },
+      // Remove redirect for /de to /de
+      {
+        source: '/de/',
+        destination: '/de',
+        permanent: false,
       },
     ];
   },
+  trailingSlash: false,
 };
+
+export default nextConfig;
+
+
+
+// module.exports = {
+//   i18n: {
+//     locales: ['de', 'en'],
+//     defaultLocale: 'de',
+//     localeDetection: false,
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/:locale',
+//         destination: '/:locale/home',
+//       },
+//     ];
+//   },
+// };
 
